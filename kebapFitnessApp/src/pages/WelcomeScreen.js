9441239a780f9c239d8972de2aeced0bc8e5eafa
Kerem Mert <kerem37mert@yahoo.com>
2023-02-03 21:07:48 +0300

    kebapFitnes Finish

---
+++ kebapFitnessApp/src/pages/WelcomeScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+
+function Welcome()
+{
+    return (
+        <SafeAreaView>
+            <Text>Welcome</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Welcome;
